9fa280285fbfc994deb7485543bb16d5161c1912
Nigamatdyanov Arthur <r2rufa@gmail.com>
2020-10-17 16:41:38 +0500

    методы класса SQLConnect и поле connection сделал нестатическими

--- src/main/java/GuestInteractor.java
+++ src/main/java/GuestInteractor.java
@@ -3,10 +3,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -48,7 +47,7 @@ public class GuestInteractor {
             case DELETE: bot.sendMsg(chatId,"напишите номер заметки, которую нужно удалить");
             guest.setCurrentCommand(botCommand);
             break;
-            case LIST: sendNotesList(guest);
+            case LIST: sendingNotesList(guest);
             sendDefaultMessage(chatId);
             guest.setCurrentCommand(BotCommands.NONE);
             break;
@@ -60,67 +59,52 @@ public class GuestInteractor {
         long chatId = guest.getChatId();
         BotCommands currentCommand = guest.getCurrentCommand();
         if(currentCommand == BotCommands.ADD){
-            addNote(messageText, guestId, chatId);
+            addingNote(messageText, guestId, chatId);
         } else if(currentCommand == BotCommands.DELETE){
-            boolean resetCurrentCommand = deleteNote(messageText, guestId, chatId);
+            int noteId = getNoteIdFromMessage(messageText);
+            if(noteId == 0) {
+                sendWrongNumberMessage(chatId);
+                return;
+            }
+            boolean resetCurrentCommand = deletingNote(noteId, guestId, chatId);
             if (!resetCurrentCommand) return;
         }
         sendDefaultMessage(chatId);
         guest.setCurrentCommand(BotCommands.NONE);
     }
 
-    static void sendDatabaseFailNotification(long chatId){
+    private static void sendDatabaseFailNotification(long chatId){
         bot.sendMsg(chatId, "не удалось подключиться к базе данных.");
     }
 
-    public static void sendDefaultMessage(long chatId){
+    private static void sendDefaultMessage(long chatId){
         bot.sendMsg(chatId, "Для управления заметками используйте следующие команды:\n " +
                 "/add - для добавления заметки \n " +
                 "/delete - для удаления заметки \n " +
                 "/list - для получения всех заметок.");
     }
 
-    public static void sendNotesList(Guest guest) {
-        StringBuilder result = new StringBuilder();
+    private static void sendingNotesList(Guest guest) {
         long chatId = guest.getChatId();
-        try (Connection connection = SQLConnect.getConnection()){
-            PreparedStatement statementNotesList = connection.prepareStatement(
-                    "SELECT note, noteid FROM guests_notes WHERE userid = ?");
-            statementNotesList.setString(1, String.valueOf(guest.getId()));
-            ResultSet resultSet = statementNotesList.executeQuery();
-            while (resultSet.next()){
-                String note = resultSet.getString("note");
-                String noteId = resultSet.getString("noteid");
-                result.append(String.format("(%s) %s \n", noteId, note));
+        try (SQLConnect sqlConnect = new SQLConnect()){
+            List<String> notesList = sqlConnect.getNotesList(guest.getId());
+            for(String message: notesList){
+                bot.sendMsg(chatId, message);
             }
-            result = new StringBuilder((result.length() == 0) ? "заметки отсутствуют!" : result.toString());
         } catch (DatabaseConnectionException | SQLException e){
             sendDatabaseFailNotification(chatId);
             if(e instanceof SQLException){
                 logger.info("Couldn't execute query:" + e.toString());
             }
         }
-        bot.sendMsg(chatId, result.toString());
+
     }
 
-    static void addNote(String messageText, int guestId, long chatId){
+    private static void addingNote(String messageText, int guestId, long chatId){
         //добавляем запись в базу, если успешно - сообщаем что запись добавлена
-        try (Connection connection = SQLConnect.getConnection()){
-            int lastNoteId = 1;
-            PreparedStatement statementLastNoteId = connection.prepareStatement(
-                    "SELECT max(noteId) FROM guests_notes WHERE userid = ? GROUP BY userid");
-            statementLastNoteId.setString(1, String.valueOf(guestId));
-            ResultSet resultSet = statementLastNoteId.executeQuery();
-            if (resultSet.next()){
-                lastNoteId = resultSet.getInt("max(noteId)");
-                lastNoteId++;
-            }
-            PreparedStatement statementInsertNote = connection.prepareStatement(
-                    "INSERT INTO guests_notes VALUES(?, ?, ?);");
-            statementInsertNote.setString(1, String.valueOf(guestId));
-            statementInsertNote.setString(2, messageText);
-            statementInsertNote.setString(3, String.valueOf(lastNoteId));
-            statementInsertNote.execute();
+        try (SQLConnect sqlConnect = new SQLConnect()){
+            int noteId = sqlConnect.getNewNoteId(guestId);
+            sqlConnect.addNote(guestId, noteId, messageText);
             bot.sendMsg(chatId, "заметка сохранена.");
         } catch (SQLException | DatabaseConnectionException e) {
             sendDatabaseFailNotification(chatId);
@@ -130,51 +114,49 @@ public class GuestInteractor {
         }
     }
 
-    static boolean deleteNote(String messageText, int guestId, long chatId){
+    private static boolean deletingNote(int noteId, int guestId, long chatId){
         //парсим в сообщении число int, ищем сообщение, если нашли удаляем, пишем что удалено успешно,
         // иначе выводим сообщение что заметка не найдена
-        try(Connection connection = SQLConnect.getConnection()){
-            int noteId = Integer.parseInt(messageText);
-            PreparedStatement statementGetRemovableNote = connection.prepareStatement(
-                    "SELECT note FROM guests_notes WHERE userid = ? AND noteid = ?");
-            statementGetRemovableNote.setString(1, String.valueOf(guestId));
-            statementGetRemovableNote.setString(2, String.valueOf(noteId));
-            ResultSet resultSet = statementGetRemovableNote.executeQuery();
-            if(!resultSet.next()){
+        try(SQLConnect sqlConnect = new SQLConnect()){
+            if(!sqlConnect.noteExists(guestId, noteId)){
                 bot.sendMsg(chatId, "не найдена заметка под данным номером, попробуйте снова.");
                 return false;
             }
+            Connection connection = sqlConnect.getConnection();
             connection.setAutoCommit(false);
             try {
-                PreparedStatement statementDeletingNote = connection.prepareStatement(
-                        "DELETE FROM guests_notes WHERE userid = ? AND noteid = ?");
-                statementDeletingNote.setString(1, String.valueOf(guestId));
-                statementDeletingNote.setString(2, String.valueOf(noteId));
-                statementDeletingNote.execute();
-                PreparedStatement statementDecrementNotesId = connection.prepareStatement(
-                        "UPDATE guests_notes SET noteid = noteid - 1 WHERE userid = ? AND noteid > ?");
-                statementDecrementNotesId.setString(1, String.valueOf(guestId));
-                statementDecrementNotesId.setString(2, String.valueOf(noteId));
-                statementDecrementNotesId.execute();
+                sqlConnect.deleteNote(guestId, noteId);
+                sqlConnect.decrementNotesId(guestId, noteId);
                 connection.commit();
             } catch (SQLException e){
                 connection.rollback();
-                logger.info("Couldn't execute queue of queries:" + e.toString());
+                logger.info("Couldn't execute one of queries in transaction:" + e.toString());
                 sendDatabaseFailNotification(chatId);
                 return false;
             } finally {
                 connection.setAutoCommit(true);
             }
             bot.sendMsg(chatId, "заметка удалена.");
-        } catch (NumberFormatException | SQLException | DatabaseConnectionException e){
-            if(e instanceof NumberFormatException) {
-                bot.sendMsg(chatId, "введите номер заметки, которую хотите удалить. Сообщение должно содержать только цифры.");
-                return false;
-            } else if(e instanceof SQLException){
+        } catch (SQLException | DatabaseConnectionException e){
+            if(e instanceof SQLException){
                 logger.info("Couldn't execute query:" + e.toString());
             }
             sendDatabaseFailNotification(chatId);
         }
         return true;
     }
+
+    private static int getNoteIdFromMessage(String messageText){
+        try{
+            return Integer.parseInt(messageText);
+        } catch (NumberFormatException e){
+            return 0;
+        }
+    }
+
+    private static void sendWrongNumberMessage(long chatId){
+        bot.sendMsg(chatId, "введите номер заметки, которую хотите удалить. Сообщение должно содержать только цифры.");
+    }
+
+
 }
--- src/main/java/SQLConnect.java
+++ src/main/java/SQLConnect.java
@@ -1,31 +1,40 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class SQLConnect {
+public class SQLConnect implements AutoCloseable {
     private static final String url = "jdbc:mysql://localhost:3306/telegrambotguests?useUnicode=true&serverTimezone=UTC";
     private static final String user = "root";
     private static final String password = "root";
-    private static Connection connection;
+    private Connection connection;
     private static final Logger logger = LoggerFactory.getLogger(SQLConnect.class);
 
-    public static Connection getConnection() throws DatabaseConnectionException {
+    @Override
+    public void close() throws SQLException {
+        connection.close();
+    }
+
+    public SQLConnect() throws DatabaseConnectionException {
+        connect();
+    }
+
+    public Connection getConnection() throws DatabaseConnectionException {
         try {
             if(connection==null||connection.isClosed()){
                 connect();
             }
         } catch (SQLException e) {
-            logger.info("Error checking if connection is closed: " + e.toString());
+            logger.info("Error checking of connection is closed: " + e.toString());
             throw new DatabaseConnectionException();
         }
         return connection;
     }
 
-    public static void connect() throws DatabaseConnectionException {
+    public void connect() throws DatabaseConnectionException {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
@@ -42,4 +51,73 @@ public class SQLConnect {
         }
     }
 
+    public boolean noteExists(int guestId, int noteId) throws SQLException {
+        PreparedStatement statementGetRemovableNote = connection.prepareStatement(
+                "SELECT note FROM guests_notes WHERE userid = ? AND noteid = ?");
+        statementGetRemovableNote.setString(1, String.valueOf(guestId));
+        statementGetRemovableNote.setString(2, String.valueOf(noteId));
+        ResultSet resultSet = statementGetRemovableNote.executeQuery();
+        return resultSet.next();
+    }
+
+    public void deleteNote(int guestId, int noteId) throws SQLException {
+        PreparedStatement statementDeletingNote = connection.prepareStatement(
+                "DELETE FROM guests_notes WHERE userid = ? AND noteid = ?");
+        statementDeletingNote.setString(1, String.valueOf(guestId));
+        statementDeletingNote.setString(2, String.valueOf(noteId));
+        statementDeletingNote.execute();
+    }
+
+    public void decrementNotesId(int guestId, int noteId) throws SQLException {
+        PreparedStatement statementDecrementNotesId = connection.prepareStatement(
+                "UPDATE guests_notes SET noteid = noteid - 1 WHERE userid = ? AND noteid > ?");
+        statementDecrementNotesId.setString(1, String.valueOf(guestId));
+        statementDecrementNotesId.setString(2, String.valueOf(noteId));
+        statementDecrementNotesId.execute();
+    }
+
+    public int getNewNoteId(int guestId) throws SQLException {
+        int noteId = 1;
+        PreparedStatement statementLastNoteId = connection.prepareStatement(
+                "SELECT max(noteId) FROM guests_notes WHERE userid = ? GROUP BY userid");
+        statementLastNoteId.setString(1, String.valueOf(guestId));
+        ResultSet resultSet = statementLastNoteId.executeQuery();
+        if (resultSet.next()){
+            noteId += resultSet.getInt("max(noteId)");
+        }
+        return noteId;
+    }
+
+    public void addNote(int guestId, int noteId, String note) throws SQLException {
+        PreparedStatement statementInsertNote = connection.prepareStatement(
+                "INSERT INTO guests_notes VALUES(?, ?, ?);");
+        statementInsertNote.setString(1, String.valueOf(guestId));
+        statementInsertNote.setString(2, note);
+        statementInsertNote.setString(3, String.valueOf(noteId));
+        statementInsertNote.execute();
+    }
+
+    public List<String> getNotesList(int guestId) throws SQLException {
+        List<String> notesList = new ArrayList<>();
+        StringBuilder nextString = new StringBuilder();
+        PreparedStatement statementNotesList = connection.prepareStatement(
+                "SELECT note, noteid FROM guests_notes WHERE userid = ?");
+        statementNotesList.setString(1, String.valueOf(guestId));
+        ResultSet resultSet = statementNotesList.executeQuery();
+        while (resultSet.next()){
+            String note = resultSet.getString("note");
+            String noteId = resultSet.getString("noteid");
+            String subString = (String.format("(%s) %s \n", noteId, note));
+            if(nextString.length() + subString.length() > 1000){
+                notesList.add(nextString.toString());
+                nextString = new StringBuilder(subString);
+            } else {
+                nextString.append(subString);
+            }
+        }
+        if(notesList.size() == 0){
+            notesList.add("заметки отсутствуют!");
+        }
+        return notesList;
+    }
 }
